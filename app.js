a7033ab2e33941da4c04ee2c5c65507a63767dd6
Tejas Joshi <tejasj293@gmail.com>
2019-04-28 04:56:36 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.get("/signup", function(req, res) {
+    res.render("signup");
+});
+
+app.listen(process.env.PORT, process.env.IP, function () {
+    console.log("SERVER STARTED");
+});
