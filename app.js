bb6430f2363dbfec35bd0c744d76450a0db452f5
Tejas Joshi <tejasj293@gmail.com>
2019-05-16 17:29:51 +0000

    16 May

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+// ============================== ROUTES =======================================
 
 app.get("/", function(req, res) {
+    res.render("landing");
+});
+
+app.get("/browse", function(req, res) {
     res.render("index");
 });
 
@@ -16,6 +23,8 @@ app.get("/login", function(req, res) {
 app.get("/signup", function(req, res) {
     res.render("signup");
 });
+
+// ====================== SERVER CONFIG ========================================
 
 app.listen(process.env.PORT, process.env.IP, function () {
     console.log("SERVER STARTED");
